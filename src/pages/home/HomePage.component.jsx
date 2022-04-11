1035cac1de96f9687d13cbbe6824959516e96907
NobiSamid <nobisamid@gmail.com>
2022-04-12 02:46:43 +0600

    basic page

--- src/pages/home/HomePage.component.jsx
+++ src/pages/home/HomePage.component.jsx
@@ -1,19 +1,6 @@
 import React from "react";
-import { Grid } from "@mui/material";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
 import { Box } from "@mui/system";
 import Directory from "../../components/directory/directory.component";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#50C878",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  height: "5rem",
-  boxShadow: "0 8px 8px 2px rgba(15, 255, 80, .5)",
-}));
 
 export default function HomePage() {
   return (
